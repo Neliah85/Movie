ffc9f8bde349c1b1937002361a05daa0de78728e
Zsuzsanna <lengyel.zsuzsanna@gmail.com>
2024-12-14 13:04:42 +0100

    Create RendezoDTO.cshtml, and RendezoDTO.cs. Write code.

---
+++ Movie/Controllers/RendezoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie.Controllers
+{
+    public class RendezoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Movie/DTOs/RendezoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Movie.DTOs
+{
+    public class RendezoDTO
+    {
+        public int? Id { get; set; }
+        public string? Nev { get; set; }
+        public string? Nemzetiseg { get; set; }
+        public DateOnly? Szuletesidatum { get; set; }        
+    }
+}
